3d02fd7ce85dc058058f97f400a4c3b027e301a3
Austin Reifsteck <apreifsteck@gmail.com>
2024-10-24 02:17:56 -0400

    removed some unneeded comments

--- hosts/work.nix
+++ hosts/work.nix
@@ -37,8 +37,6 @@ in {
       "asdf"
     ];
 
-    home.file.".obsidian.vimrc".target = "Documents/personal/General/.obsidian.vimrc";
-
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
     # introduces backwards incompatible changes.
--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -3,19 +3,10 @@
   # $ nix-env -qaP | grep wget
   environment.systemPackages = [ ];
 
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
-
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
-
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true; # default shell on catalina
 
   programs.direnv = {
-    # enableZshIntegration = true;
     enable = true;
     nix-direnv = {
       enable = true;
